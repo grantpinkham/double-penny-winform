7a9d3c1a8917e573c342485222c75d1729352444
grantpinkham <grantpinkham@yahoo.com>
2022-03-23 09:36:02 -0500

    Add project files.

---
+++ DoublePennies/DoublePenniesForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DoublePennies
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSum = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumOfDays = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumOfDays)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(233, 216);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(215, 51);
+            this.btnSum.TabIndex = 0;
+            this.btnSum.Text = "Sum Pennies";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(233, 343);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(215, 31);
+            this.txtTotal.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total Number of Pennies per Number of Day(s)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(416, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter # of Days, (1st day does not double)";
+            // 
+            // txtNumOfDays
+            // 
+            this.txtNumOfDays.Location = new System.Drawing.Point(233, 114);
+            this.txtNumOfDays.Name = "txtNumOfDays";
+            this.txtNumOfDays.Size = new System.Drawing.Size(215, 31);
+            this.txtNumOfDays.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select Sum Pennies";
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 420);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumOfDays);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.btnSum);
+            this.Name = "frmMainForm";
+            this.Text = "Double Your Pennies Each Subsequent Day";
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumOfDays)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown txtNumOfDays;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DoublePennies/DoublePenniesForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoublePennies
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int numOfPennies = 1;
+            int sum = 1;
+            int numOfDays = Convert.ToInt32(txtNumOfDays.Value);
+            for (int days = 2; days <= numOfDays; days++)
+            {
+                numOfPennies *= 2;
+                sum += numOfPennies;
+            }
+            txtTotal.Text = sum.ToString();
+        }
+    }
+}
